af35ab677a2a098b06843084efb2dc1799b9b52a
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-09 10:24:37 -0800

    Models and resources have been generated

---
+++ db/migrate/20200109181953_create_trips.rb
@@ -0,0 +1,11 @@
+class CreateTrips < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trips do |t|
+      t.string :name
+      t.string :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200109182225_create_list_items.rb
@@ -0,0 +1,11 @@
+class CreateListItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :list_items do |t|
+      t.string :content
+      t.boolean :isChecked
+      t.integer :list_id
+
+      t.timestamps
+    end
+  end
+end
