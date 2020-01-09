09b29e7aa8ab965b7189bd4ef1d7b89b8a4a580c
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-09 10:35:09 -0800

    Added relationships to models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+    belongs_to :trip
+    has_many :list_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :trips
+    validates :username, uniqueness: true
 end
