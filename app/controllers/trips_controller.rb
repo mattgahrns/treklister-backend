6b08bc867bb892392dcec1618775a9a119fef143
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-10 13:27:15 -0800

    Implemented a user being able to create a new Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,44 @@
 class TripsController < ApplicationController
+
+    def create
+        trip = Trip.new(trip_params)
+        user = User.find_by(id: params[:id])
+        trip.user_id = user.id
+        if trip.save
+            beforeList = List.create(before: true, trip_id: trip.id)
+            afterList = List.create(before: false, trip_id: trip.id)
+            render json: trip, include: [:user]
+        else
+            render json: { message: 'Post could not be created. Please try again.' }
+        end
+    end
+
+    def index
+        trips = Trip.all
+        render json: trips, include: [:user]
+    end
+
+    def show
+        trip = Trip.find_by(id: params[:id])
+        render json: trip, include: [:user]
+    end
+
+    def update
+        trip = Trip.find_by(id: params[:id])
+        if ptripupdate(trip_params)
+            render json: { message: 'Trip updated!' }
+        else
+            render json: { message: 'Trip could not be edited. Please try again.' }
+        end
+    end
+
+    def destroy
+        trip = Trip.find_by(id: params[:id])
+        trip.destroy
+    end
+
+    private
+    def trip_params
+        params.require(:trip).permit(:name)
+    end
 end
